5d502c0da0ebf7ed16b245cbdc27b7ec3b7b0bff
haltim <timoayomide2000@gmail.com>
2022-07-02 09:34:12 +0100

    MERN backend

---
+++ mernserver/public/static/precache-manifest.22779c7465b74037f79b4b2389c8b0f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c07b0d6d8118e8b4425e",
+    "url": "/static/css/main.1ed6aebc.chunk.css"
+  },
+  {
+    "revision": "c07b0d6d8118e8b4425e",
+    "url": "/static/js/main.c07b0d6d.chunk.js"
+  },
+  {
+    "revision": "44da05becf7e2716d148",
+    "url": "/static/js/1.44da05be.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ea7af5422a79cc21460c8ebb62c23ade",
+    "url": "/static/media/blog.ea7af542.jpg"
+  },
+  {
+    "revision": "e4af2228da19f0226f86c0c608c6638e",
+    "url": "/index.html"
+  }
+];
