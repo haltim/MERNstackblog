eb8b0bfd29093f4e22375eb6bc4d805938353be5
haltim <timoayomide2000@gmail.com>
2022-07-02 09:26:18 +0100

    MERNstack

---
+++ mernserver/public/precache-manifest.ab2900e102a9707f8822dc13f08b3580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "24356c354b00e2a300d1",
+    "url": "/static/css/main.0cd50edf.chunk.css"
+  },
+  {
+    "revision": "24356c354b00e2a300d1",
+    "url": "/static/js/main.24356c35.chunk.js"
+  },
+  {
+    "revision": "a2bb5abad0cf16d1bd18",
+    "url": "/static/js/1.a2bb5aba.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ea7af5422a79cc21460c8ebb62c23ade",
+    "url": "/static/media/blog.ea7af542.jpg"
+  },
+  {
+    "revision": "78a95069dde3b862bd73fbae91a1166c",
+    "url": "/index.html"
+  }
+];
---
+++ mernserver/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+import dotenv from "dotenv";
+import path from "path";
+import {fileURLToPath} from 'url';
+
+
+dotenv.config();
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname + "/mernclientpage/build")))
+
+
+
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+app.use('/posts', postRoutes);
+
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static("/public"));
+}
+
+
+const PORT  =process.env.PORT || 5000
+const url = process.env.MONGODB_URI
+
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Connected to database`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+  
